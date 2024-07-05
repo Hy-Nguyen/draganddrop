7b8ef26465e90fb54404442632e36f03d6823503
Hy Nguyen <giahy316@gmail.com>
2024-07-04 18:49:01 -0700

    added sortbale functionality

--- src/components/Bento/BentoContainer.tsx
+++ src/components/Bento/BentoContainer.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/lib/utils';
 import { useDroppable } from '@dnd-kit/core';
+import { SortableContext } from '@dnd-kit/sortable';
 
 interface BentoContainerProps {
   id: string;
@@ -15,11 +16,11 @@ export default function BentoContainer({ id, children, className }: BentoContain
     <div
       ref={setNodeRef}
       className={cn(
-        'bg-black-wd outline-green-wd grid h-full w-full grid-cols-12 grid-rows-9 items-center justify-center gap-4 rounded-2xl p-12 outline',
+        'grid h-full w-full grid-cols-12 grid-rows-9 items-center justify-center gap-4 rounded-2xl bg-black-wd p-12 outline outline-green-wd',
         className
       )}
     >
-      {children}
+      <SortableContext items={[]}>{children}</SortableContext>
     </div>
   );
 }
